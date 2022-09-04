99a6f168558dfbf6d179b8cd48e1fcd66a595937
Lev135 <lev_135@mail.ru>
2022-09-04 11:56:25 +0300

    haddock to Bidirectional.TH added

--- src/Control/Bidirectional/TH.hs
+++ src/Control/Bidirectional/TH.hs
@@ -8,21 +8,64 @@
   Maintainer:    lev_135@mail.ru
   Stability:     experimental
 -}
-module Control.Bidirectional.TH where
+module Control.Bidirectional.TH (
+    decBidirectionalInstances, 
+    makeBidirectionalInstances
+  ) where
 import Language.Haskell.TH (Exp, Q, Dec (InstanceD, ClassD), TypeQ, DecsQ, reify, Info (ClassI), Type (ConT, AppT, TupleT), conT)
 import Control.Bidirectional.Class (Bidirectional, BidirectionalRec, Constr, ConstrRec)
 import Control.Monad (join)
 
+{-| 
+  Declare instance and make it bidirectional at the same time.
+  Provides instances for 'Bidirectional' and 'BidirectionalRec'.
+
+  It's suitable for declaring your own instances. To make existing instances
+  (for example, from libs) bidirectional, use 'makeBidirectionalInstances'.
+
+  You can use it for declaring multiple instances:
+
+  > data A a = A a
+  > data B a b = B a b
+  > data C a b = CA a | CB b
+  > 
+  > decBidirectionalInstances [d| 
+  >     instance Show a => Show (A a) where
+  >       show (A a) = "A " ++ show a
+  >     instance (Show a, Show b) => Show (B a b) where
+  >       show (B a b) = "B " ++ show a ++ " " show b
+  >     instance (Show a, Show b) => Show (C a b) where
+  >       show (CA a) = "CA " ++ show a
+  >       show (CB b) = "CB " ++ show b
+  >   |] 
+-} 
 decBidirectionalInstances :: Q [Dec] -> Q [Dec]
 decBidirectionalInstances instances = do
   inst <- instances
   (inst <>) <$> makeBidirectionalInstances instances
 
+{- |
+  Make existing instance bidirectional.
+  Provides instances for 'Bidirectional' and 'BidirectionalRec'.
+
+  It's suitable for making bidirectional existing instances, that you can't 
+  change (for example, from libs). If you want to declare your one instance
+  and make it bidirectional, use 'decBidirectionalInstances'.
+
+  You can use it for declaring multiple instances:
+
+  > makeBidirectionalInstances [d| 
+  >     instance Show a => Show [a]
+  >     instance (Show a, Show b) => Show (a, b)
+  >     instance (Show a, Show b) => Show (Either a b)
+  >   |] 
+  
+  Note that you need not provide the body of instance, only its head. 
+-}
 makeBidirectionalInstances :: Q [Dec] -> Q [Dec]
 makeBidirectionalInstances instances = do
   insts <- instances
   join <$> mapM makeBidirectionalInstance insts
-
 
 makeBidirectionalInstance :: Dec -> Q [Dec]
 makeBidirectionalInstance inst = do
